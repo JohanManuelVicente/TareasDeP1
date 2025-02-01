5738d1cf7cd853adb25cb293030e260ca637f266
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-01-31 23:01:17 -0400

    Subiendo Primer Codigo de C# de la tarea 1
    Declarando variables en C# por Visual Studio

--- Proyectos General C#/Codigosbasicos/CodigosDeTarea1/Program.cs
+++ Proyectos General C#/Codigosbasicos/CodigosDeTarea1/Program.cs
@@ -1,2 +1,20 @@
-﻿sdsdsdsd
-    sdsdsd
+﻿//Johan Manuel Vicente 2024-1260
+
+int IntNumber = 25;
+decimal DecimalNumber = 3.145454m;
+double DoubleNumber = 3.14;
+float FloatNumber = 2.5f;
+char character = 'A';
+string text = "Hola a todos";
+bool isTrue = true;
+
+
+Console.WriteLine("Número Entero: " + IntNumber);
+Console.WriteLine("Número Decimal : " + DecimalNumber);
+Console.WriteLine("Número Double : " + DoubleNumber);
+Console.WriteLine("Número Flotante : " + FloatNumber);
+Console.WriteLine("Carácter: " + character);
+Console.WriteLine("Texto: " + text);
+Console.WriteLine("Booleano: " + isTrue);
+
+Console.ReadKey();
