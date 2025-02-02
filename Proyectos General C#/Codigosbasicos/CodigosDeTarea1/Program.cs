f2de11523cb11ff18ed825f14443681ef8b1b127
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-02-02 01:41:08 -0400

    Probando el ejercicio 4 de la tarea 1 de P1

--- Proyectos General C#/Codigosbasicos/CodigosDeTarea1/Program.cs
+++ Proyectos General C#/Codigosbasicos/CodigosDeTarea1/Program.cs
@@ -1,25 +1,33 @@
-﻿//Johan Manuel Vicente 2024-1260
+﻿////Johan Manuel Vicente 2024-1260
 
-var number = 7;
-number++;
-Console.WriteLine(number);
+//var number = 7;
+//number++;
+//Console.WriteLine(number);
 
-number--;
-Console.WriteLine(number);
+//number--;
+//Console.WriteLine(number);
 
-number+= 7;
-Console.WriteLine(number);
+//number+= 7;
+//Console.WriteLine(number);
 
-number-= 4;
-Console.WriteLine(number);
+//number-= 4;
+//Console.WriteLine(number);
 
-number*=2;
-Console.WriteLine(number);
+//number*=2;
+//Console.WriteLine(number);
 
-number/=4;
-Console.WriteLine(number);
+//number/=4;
+//Console.WriteLine(number);
 
-number%= 2;
-Console.WriteLine(number);
+//number%= 2;
+//Console.WriteLine(number);
+
+float number = 10152466.25F;
+
+byte BYTE = 5;
+
+var numberByte = (byte)number;
+
+Console.WriteLine(numberByte);
 
 Console.ReadKey();
