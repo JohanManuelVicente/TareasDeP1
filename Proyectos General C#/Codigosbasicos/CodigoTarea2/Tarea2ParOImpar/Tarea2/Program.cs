da4bc5ee03e49839a23659a37683425044067cd1
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-02-02 02:23:10 -0400

    Subiendo Tarea 2 de P1 si un number en par o impar

---
+++ Proyectos General C#/Codigosbasicos/CodigoTarea2/Tarea2ParOImpar/Tarea2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Johan Manuel Vicente    2024-1260
+Console.Write("Introduzca un número entero: ");
+string input = Console.ReadLine(); // Leer la entrada como string
+int number;
+
+// convertir la entrada string a un número entero
+// convertir la entrada string a un número entero
+if (int.TryParse(input, out number))
+{
+    // Verificar si el número es par o impar
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("El número " + number + " es PAR.");
+    }
+    else
+    {
+        Console.WriteLine("El número " + number + " es IMPAR.");
+    }
+}
+else
+
+    Console.WriteLine("Error: No ingresaste un número entero válido.");
+
+Console.ReadKey();
+
