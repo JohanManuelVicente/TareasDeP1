629a7924893ea551c639ff315d6277b271a24127
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-02-02 01:23:17 -0400

    Declarando variable y decrementarlo e incrementarlo y haciendo operaciones con ello

--- Proyectos General C#/Codigosbasicos/CodigosDeTarea1/Program.cs
+++ Proyectos General C#/Codigosbasicos/CodigosDeTarea1/Program.cs
@@ -1,20 +1,25 @@
 ﻿//Johan Manuel Vicente 2024-1260
 
-int IntNumber = 25;
-decimal DecimalNumber = 3.145454m;
-double DoubleNumber = 3.14;
-float FloatNumber = 2.5f;
-char character = 'A';
-string text = "Hola a todos";
-bool isTrue = true;
+var number = 7;
+number++;
+Console.WriteLine(number);
 
+number--;
+Console.WriteLine(number);
 
-Console.WriteLine("Número Entero: " + IntNumber);
-Console.WriteLine("Número Decimal : " + DecimalNumber);
-Console.WriteLine("Número Double : " + DoubleNumber);
-Console.WriteLine("Número Flotante : " + FloatNumber);
-Console.WriteLine("Carácter: " + character);
-Console.WriteLine("Texto: " + text);
-Console.WriteLine("Booleano: " + isTrue);
+number+= 7;
+Console.WriteLine(number);
+
+number-= 4;
+Console.WriteLine(number);
+
+number*=2;
+Console.WriteLine(number);
+
+number/=4;
+Console.WriteLine(number);
+
+number%= 2;
+Console.WriteLine(number);
 
 Console.ReadKey();
